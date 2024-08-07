9dcad0ea856be54ed2463fe7c9938cb9fbf5a3c3
Daniel Souza <workdndsza@gmail.com>
2024-08-07 15:28:11 -0300

    movendo as validacoes para o dto

--- src/main/java/br/com/dnsouzadev/adopet/api/dto/SolicitacaoAdocaoDto.java
+++ src/main/java/br/com/dnsouzadev/adopet/api/dto/SolicitacaoAdocaoDto.java
@@ -1,8 +1,11 @@
 package br.com.dnsouzadev.adopet.api.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record SolicitacaoAdocaoDto(
-        Long idTutor,
-        Long idPet,
-        String motivo
+        @NotNull Long idTutor,
+        @NotNull Long idPet,
+        @NotBlank String motivo
 ) {
 }
